ba31c8e7fdde0c602a41ee9e53b362ffe83b004f
macnack <krupka.maciej@icloud.com>
2022-01-26 19:59:10 +0100

    write all files to dataframe

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 import pandas as pd
 import xml.etree.ElementTree as ET
-from pathlib import Path
+import os
 anno_test_path = "./test/annotations/road4.xml"
+anno_test_path1 = "./test/annotations/road2.xml"
 img_test_path = "./test/images/"
-
+path = "./test/annotations/"
 anno_train_path = "./train/annotations/"
 img_train_path = "./train/images/"
-images_path = Path('./test/images')
-anno_path = Path('./test/annotations')
 
+# select object from a file to pandas row
 def make_data_row( file_path ):
-    test =ET.parse( anno_test_path ).getroot()
+    test =ET.parse( file_path ).getroot()
     items = []
     for name in test.findall("./object"):
         filename_ = test.find("./filename").text
@@ -28,9 +28,33 @@ def make_data_row( file_path ):
         xmax_ = name.find("./bndbox/xmax").text
         ymax_ = name.find("./bndbox/ymax").text
         items.append({'filename': filename_, 'width': width_,
-                      'heigh_': heigh_, 'class' : class_,
+                      'heigh': heigh_, 'class' : class_,
                       'xmin' : xmin_, 'ymin': ymin_,
                       'xmax': xmax_, 'ymax': ymax_})
     return items
-df_train = make_data_row(anno_test_path)
-print(pd.DataFrame(df_train))
+# from folder create pandas data frame with objects
+def make_frame( path ):
+    frame = pd.DataFrame()
+    items = []
+    for entry in os.listdir(path):
+        file_path = os.path.join(path, entry)
+        if os.path.isfile(file_path):
+            if '.xml' in file_path:
+                    test =ET.parse( file_path ).getroot()
+                    for name in test.findall("./object"):
+                        filename_ = test.find("./filename").text
+                        width_ = test.find("./size/width").text
+                        heigh_ = test.find("./size/height").text
+                        class_ = name.find("./name").text
+                        xmin_ = name.find("./bndbox/xmin").text
+                        ymin_ = name.find("./bndbox/ymin").text
+                        xmax_ = name.find("./bndbox/xmax").text
+                        ymax_ = name.find("./bndbox/ymax").text
+                        items.append({'filename': filename_, 'width': width_,
+                                        'heigh': heigh_, 'class' : class_,
+                                        'xmin' : xmin_, 'ymin': ymin_,
+                                        'xmax': xmax_, 'ymax': ymax_})
+    return pd.DataFrame(items)
+
+df_train = make_frame(path)
+print(df_train.sort_values(by='filename'))
