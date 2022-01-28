dec1db87400f44d3aeda4563edd67af5ea8ef99c
macnack <krupka.maciej@icloud.com>
2022-01-28 21:22:10 +0100

    split data

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 import pandas as pd
 import xml.etree.ElementTree as ET
+from pathlib import Path
 import os
-anno_test_path = "./test/annotations/road4.xml"
-anno_test_path1 = "./test/annotations/road2.xml"
+import random
+
+anno_test_path = "./test/annotations/"
 img_test_path = "./test/images/"
-path = "./train/annotations/"
+
 anno_train_path = "./train/annotations/"
 img_train_path = "./train/images/"
 
+data_xml_path = Path("./dataset/annotations/")
+data_img_path = Path("./dataset/images/")
+
+
 # select object from a file to pandas row
-def make_data_row( file_path ):
-    test =ET.parse( file_path ).getroot()
+def make_data_row(file_path):
+    test = ET.parse(file_path).getroot()
     items = []
     for name in test.findall("./object"):
         filename_ = test.find("./filename").text
@@ -28,56 +34,54 @@ def make_data_row( file_path ):
         xmax_ = name.find("./bndbox/xmax").text
         ymax_ = name.find("./bndbox/ymax").text
         items.append({'filename': filename_, 'width': width_,
-                      'heigh': heigh_, 'class' : class_,
-                      'xmin' : xmin_, 'ymin': ymin_,
+                      'heigh': heigh_, 'class': class_,
+                      'xmin': xmin_, 'ymin': ymin_,
                       'xmax': xmax_, 'ymax': ymax_})
     return items
+
+
 # from folder create pandas data frame with objects
-def make_frame( path ):
-    frame = pd.DataFrame()
+def make_frame(path):
     items = []
     for entry in os.listdir(path):
         file_path = os.path.join(path, entry)
         if os.path.isfile(file_path):
             if '.xml' in file_path:
-                    test =ET.parse( file_path ).getroot()
-                    for name in test.findall("./object"):
-                        filename_ = test.find("./filename").text
-                        width_ = test.find("./size/width").text
-                        heigh_ = test.find("./size/height").text
-                        class_ = name.find("./name").text
-                        xmin_ = name.find("./bndbox/xmin").text
-                        ymin_ = name.find("./bndbox/ymin").text
-                        xmax_ = name.find("./bndbox/xmax").text
-                        ymax_ = name.find("./bndbox/ymax").text
-                        items.append({'filename': filename_, 'width': width_,
-                                        'heigh': heigh_, 'class' : class_,
-                                        'xmin' : xmin_, 'ymin': ymin_,
-                                        'xmax': xmax_, 'ymax': ymax_})
+                test = ET.parse(file_path).getroot()
+                for name in test.findall("./object"):
+                    filename_ = test.find("./filename").text
+                    width_ = test.find("./size/width").text
+                    heigh_ = test.find("./size/height").text
+                    class_ = name.find("./name").text
+                    xmin_ = name.find("./bndbox/xmin").text
+                    ymin_ = name.find("./bndbox/ymin").text
+                    xmax_ = name.find("./bndbox/xmax").text
+                    ymax_ = name.find("./bndbox/ymax").text
+                    items.append({'filename': filename_, 'width': width_,
+                                  'heigh': heigh_, 'class': class_,
+                                  'xmin': xmin_, 'ymin': ymin_,
+                                  'xmax': xmax_, 'ymax': ymax_})
+                    #break turn off repetition
     return pd.DataFrame(items)
+# make train dataset
+def fill_train_data( frame ): # only with no repetition
+    for key in frame['class'].value_counts().keys().tolist():
+        files = frame.loc[frame['class'] == key]
+        random_20 = files.sample(int(len(files) * 0.2))
+        file_name = random_20['filename'].value_counts().keys().tolist()
+        for item in file_name:
+            item = item[:-4]
+            img_path = os.path.join(img_train_path, item.__add__('.png'))
+            xml_path = os.path.join(anno_train_path, item.__add__('.xml'))
+            Path(img_path).rename(Path(os.path.join(img_test_path, item.__add__('.png'))))
+            Path(xml_path).rename(Path(os.path.join(anno_test_path, item.__add__('.xml'))))
+    return 1
 
-df_train = make_frame(path)
-#print( len((df_train.loc[df_train['class'] == 'speedlimit' ]['filename']).value_counts()) )
-#print( len((df_train.loc[df_train['class'] == 'speedlimit' ]['filename'])) )
-#print( df_train['filename'].value_counts().values)
-#print(len((df_train['filename'].value_counts().axes).value))
+data_frame_train = make_frame(anno_train_path)
+data_frame_test = make_frame(anno_test_path)
+# img_speedlimit = data_frame.loc[data_frame['class'] == 'speedlimit']
+# xy = (img_speedlimit['filename'].value_counts().keys().tolist(),
+#       img_speedlimit['filename'].value_counts().tolist())
 
-
-# for filename in (df_train['filename'].value_counts().axes):
-#      for x in filename.values:
-#           print(x)
-#      print(len(filename.values))
-# for y in df_train['filename'].value_counts():
-#     print(y)
-# print( df_train['filename'].value_counts() )
-img_speedlimit = df_train.loc[df_train['class'] == 'speedlimit']
-xy = ( img_speedlimit['filename'].value_counts().keys().tolist(),  img_speedlimit['filename'].value_counts().tolist() )
-# for ax in range(len(xy[0])):
-#     print( xy[0][ax] )
-#     print( xy[1][ax] )
-# print(sum(xy[1])*0.2)
-
-for ax in range(78):
-    print( xy[0][ax] )
-    print( xy[1][ax] )
-#print(df_train.value_counts())
+print(data_frame_test['heigh'].value_counts())
+print(data_frame_train['heigh'].value_counts())
