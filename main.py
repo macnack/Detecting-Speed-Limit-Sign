12012712d9d4c62a1d26d9b2de06adabf0068bb9
macnack <krupka.maciej@icloud.com>
2022-02-12 19:50:39 +0100

    cleaning code

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ import os
 from matplotlib import pyplot as plt
 import random
 
+## factorial of image
+alpha = 1 / 10
+## nnm threshold
+overlapThresh = 0.3
+
 # file paths
 anno_test_path = "./test/annotations/"
 img_test_path = "./test/images/"
@@ -21,26 +26,6 @@ img_train_path = "./train/images/"
 
 data_xml_path = Path("./dataset/annotations/")
 data_img_path = Path("./dataset/images/")
-
-
-# select object from a file to pandas dataframe
-def make_data_row(file_path):
-    test = ET.parse(file_path).getroot()
-    items = []
-    for name in test.findall("./object"):
-        filename_ = test.find("./filename").text
-        width_ = test.find("./size/width").text
-        heigh_ = test.find("./size/height").text
-        class_ = name.find("./name").text
-        xmin_ = name.find("./bndbox/xmin").text
-        ymin_ = name.find("./bndbox/ymin").text
-        xmax_ = name.find("./bndbox/xmax").text
-        ymax_ = name.find("./bndbox/ymax").text
-        items.append({'filename': filename_, 'width': width_,
-                      'heigh': heigh_, 'class': class_,
-                      'xmin': xmin_, 'ymin': ymin_,
-                      'xmax': xmax_, 'ymax': ymax_})
-    return items
 
 
 # import from xml file....
@@ -60,14 +45,15 @@ def make_frame(path):
                     ymin_ = name.find("./bndbox/ymin").text
                     xmax_ = name.find("./bndbox/xmax").text
                     ymax_ = name.find("./bndbox/ymax").text
-                    items.append({'filename': filename_, 'width': width_,
-                                  'heigh': heigh_, 'class': class_,
+                    items.append({'filename': filename_, 'width': int(width_),
+                                  'heigh': int(heigh_), 'class': class_,
                                   'xmin': int(xmin_), 'ymin': int(ymin_),
                                   'xmax': int(xmax_), 'ymax': int(ymax_)})
                     # break if turn off repetition
     return pd.DataFrame(items)
 
 
+## to test classify method
 def make_input_frame(path):
     items = []
     for entry in os.listdir(path):
@@ -96,7 +82,7 @@ def class_change(frame):
     frame['class'] = frame['class'].apply(lambda x: class_new_dict[x])
 
 
-# make train dataset
+# make random shuffle train dataset
 def fill_train_data(frame):  # only with no repetition
     for key in frame['class'].value_counts().keys().tolist():
         files = frame.loc[frame['class'] == key]
@@ -146,8 +132,6 @@ def extract_features(frame, path):
     for _, row in frame.iterrows():
         img = cv2.imread(os.path.join(path, row[0]), cv2.IMREAD_COLOR)
         grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # blur = cv2.GaussianBlur(grayscale, (7, 7), 1.5)
-        # grayscale=cv2.Canny(blur,50,100)
         if flag == False:
             grayscale = grayscale[row[5]:row[7], row[4]:row[6]]
         if flag:
@@ -198,12 +182,12 @@ def evaluate(frame):
     return
 
 
-def output(filename, n_objects, bounds):
+def output(filename, bounds):
     print(filename)
-    print(n_objects)
-    if n_objects != 0:
+    print(len(bounds))
+    if len(bounds) != 0:
         for bound in bounds:
-            print(bound[0][0], ' ', bound[1][0], ' ', bound[0][1], ' ', bound[1][1])
+            print('(', bound[0][0], ',', bound[0][1], ',', bound[1][0], ',', bound[1][1], ')')
     return
 
 
@@ -221,8 +205,57 @@ def input_classify():
     return pd.DataFrame(to_do_list)
 
 
-def detect(frame):
+def non_max_suppression(x_min, x_max, y_min, y_max):
+    choose = []
+    area = (x_max - x_min + 1) * (y_max - y_min + 1)
+    index = np.argsort(y_max)
+    while len(index) > 0:
+        last_index = len(index) - 1
+        i = index[last_index]
+        choose.append(i)
+        suppression = [last_index]
+        for pos in range(0, last_index):
+            j = index[pos]
+            width = max(0, min(x_max[i], x_max[j]) - max(x_min[i], x_min[j]) + 1)
+            height = max(0, min(y_max[i], y_max[j]) - max(y_min[i], y_min[j]) + 1)
+            overlap = float(width * height) / area[j]
+            if overlap > overlapThresh:
+                suppression.append(pos)
+        index = np.delete(index, suppression)
+    boxes = []
+    for pick in choose:
+        box = ((x_min[pick], y_min[pick]), (x_max[pick], y_max[pick]))
+        boxes.append(box)
+    return boxes
+
+def extract_and_predict_loc(rf, frame):
+    filename = frame['filename'].to_list()
+    bounds = frame['find_bounds'].to_list()
+    class_predict = []
+    for i in range(len(filename)):
+        img = cv2.imread(os.path.join(img_test_path, filename[i]), cv2.IMREAD_COLOR)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        sift = cv2.SIFT_create()
+        flannBasedMatcher = cv2.FlannBasedMatcher_create()
+        my_bow = cv2.BOWImgDescriptorExtractor(sift, flannBasedMatcher)
+        my_vocabulary = np.load('my_voc.npy')
+        my_bow.setVocabulary(my_vocabulary)
+        class_predict_ = []
+        for ((xmin, ymin), (xmax, ymax)) in bounds[i]:
+            img_gray_part = gray[ymin:ymax, xmin:xmax]
+            imageDescriptor = my_bow.compute(img_gray_part, sift.detect(img_gray_part, None))
+            if imageDescriptor is not None:
+                class_predict_.append(rf.predict(imageDescriptor)[0])
+            else:
+                class_predict_.append(None)
+        class_predict.append(class_predict_)
+    frame['class_pred'] = class_predict
+    return frame
+
+images = []
+def localization(frame):
     filename = 'road.png'
+    new_frame = []
     for _, row in frame.iterrows():
         img = cv2.imread(os.path.join(img_test_path, row[0]), cv2.IMREAD_COLOR)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -233,29 +266,32 @@ def detect(frame):
             filename = row[0]
             if circles is not None:
                 boxes = []
+                xmin_ = np.array(0)
+                ymin_ = np.array(0)
+                xmax_ = np.array(0)
+                ymax_ = np.array(0)
                 count = 0
                 for i in circles[0, :]:
-                    xmin = np.uint16(np.around(max(i[0] - i[2], 0)))
-                    ymin = np.uint16(np.around(max(i[1] - i[2], 0)))
-                    xmax = np.uint16(np.around(i[0] + i[2]))
-                    ymax = np.uint16(np.around(i[1] + i[2]))
-                    box = ((xmin, ymin), (xmax, ymax))
-                    boxes.append(box)
+                    xmin = np.array(np.uint16(np.around(max(i[0] - i[2], 0))))
+                    ymin = np.array(np.uint16(np.around(max(i[1] - i[2], 0))))
+                    xmax = np.array(np.uint16(np.around(min(i[0] + i[2], row[1]))))
+                    ymax = np.array(np.uint16(np.around(min(i[1] + i[2], row[2]))))
+                    if xmin + xmax >= alpha * img.shape[1] and ymin + ymax >= alpha * img.shape[0]:
+                        box = ((xmin, ymin), (xmax, ymax))
+                        count += 1
+                        boxes.append(box)
+                        xmin_ = np.vstack((xmin_, xmin))
+                        xmax_ = np.vstack((xmax_, xmax))
+                        ymin_ = np.vstack((ymin_, ymin))
+                        ymax_ = np.vstack((ymax_, ymax))
                     # draw the outer circle
                     if False:
                         cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
-                    ## Is this speedlimit ?? detection
-                    sift = cv2.SIFT_create()
-                    flannBasedMatcher = cv2.FlannBasedMatcher_create()
-                    my_bow = cv2.BOWImgDescriptorExtractor(sift, flannBasedMatcher)
-                    my_vocabulary = np.load('my_voc.npy')
-                    my_bow.setVocabulary(my_vocabulary)
-                    img_gray_part = gray[ymin:ymax, xmin:xmax]
-                    imageDescriptor = my_bow.compute(img_gray_part, sift.detect(img_gray_part, None))
-                    if imageDescriptor is not None:
-                        count += 1
-            output(filename, count, boxes)
-    return 1
+                    images.append(img)
+            boxes = non_max_suppression(xmin_[1:, 0], xmax_[1:, 0], ymin_[1:, 0], ymax_[1:, 0])
+            new_frame.append({'filename': filename, 'find_bounds': boxes})
+            output(filename, boxes)
+    return pd.DataFrame(new_frame)
 
 
 def output_predict(frame):
@@ -296,25 +332,22 @@ def main():
     # print(data_frame_test['class'].value_counts())
     class_change(data_frame_test)
     # print("learn bovw")
-    learn_bovw(data_frame_train)
+    # learn_bovw(data_frame_train)
     # print("extract features")
     data_frame_train = extract_features(data_frame_train, 'train')
     # print("train")
     rf = train(data_frame_train)
     print("predict")
     data_frame_test = extract_features(data_frame_test, 'test')
-    data_frame_test = predict(rf, data_frame_test)
+    # data_frame_test = predict(rf, data_frame_test)
     # evaluate(data_frame_test)
-    if input("detect or classify ( repeat ) ") == 'classify':
-        classify(rf)
-    else:
-        detect(data_frame_test.iloc[75:100])
-    # classify = make_input_frame(anno_test_path)
-    # print(classify)
-    # for _, row in classify.iterrows():
-    #     print(row[1])
-    #     ((x,xx),(y,yy)) = row[1]
-    #     print(x,xx,y,yy)
+    located = localization(data_frame_test.iloc[75:100])
+    located = extract_and_predict_loc(rf, located)
+    print(located)
+    # if input("detect or classify ( repeat ) ") == 'classify':
+    #     classify(rf)
+    # else:
+    #     localization(data_frame_test.iloc[75:100])
 
 
 # data_frame_test = extract_features( data_frame_test, 'test' )
@@ -346,7 +379,7 @@ def plot(images):
 
 
 main()
-
+plot(images)
 # detect(data_frame_test.iloc[54:79].iterrows())
 # img = cv2.imread(os.path.join(img_test_path, 'road210.png'), cv2.IMREAD_COLOR)
 # img = img[135: 156, 179: 199]
