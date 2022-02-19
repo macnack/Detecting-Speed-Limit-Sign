d71b07a3be79c38f5d28f158b804394db55547dc
macnack <krupka.maciej@icloud.com>
2022-02-19 09:14:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -476,12 +476,12 @@ def add_random_background(frame):
 def main():
     # print("Read train file")
     data_frame_train = make_frame(anno_train_path)
-    print(data_frame_train['class'].value_counts())
+    #print(data_frame_train['class'].value_counts())
     data_frame_train = add_random_background(data_frame_train)
     class_change(data_frame_train)
     # print("Read test file")
     data_frame_test = make_frame(anno_test_path)
-    print(data_frame_test['class'].value_counts())
+    #print(data_frame_test['class'].value_counts())
     class_change(data_frame_test)
     # print("learn bovw")
     learn_bovw(data_frame_train)
@@ -490,9 +490,9 @@ def main():
     # print("train")
     rf = train(data_frame_train)
     # print("predict")
-    data_frame_test = extract_features(data_frame_test, 'test')
-    data_frame_test = predict(rf, data_frame_test)
-    evaluate(data_frame_test)
+    #data_frame_test = extract_features(data_frame_test, 'test')
+    #data_frame_test = predict(rf, data_frame_test)
+    #evaluate(data_frame_test)
 
     # tekst "detect or classify ( repeat ) "
     if input(welcome_str) == 'classify':
