21f289fdea4eb2dd5b37e1e8458e79417c6f88e4
jwietrzykowski <jan.m.wietrzykowski@gmail.com>
2022-02-21 15:14:47 +0100

    Komentarze i ocena.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from matplotlib import pyplot as plt
 # TODO Jakość kodu i raport (4/4)
 
 
-# TODO Skuteczność klasyfikacji 0.0 (0/4)
+# TODO Skuteczność klasyfikacji 0.766 (3/4)
 # TODO [0.00, 0.50) - 0.0
 # TODO [0.50, 0.55) - 0.5
 # TODO [0.55, 0.60) - 1.0
@@ -24,21 +24,11 @@ from matplotlib import pyplot as plt
 # TODO [0.80, 0.85) - 3.5
 # TODO [0.85, 1.00) - 4.0
 
-# stderr:
-# Traceback (most recent call last):
-#   File "main.py", line 429, in <module>
-#     main()
-#   File "main.py", line 423, in main
-#     classify(rf)
-#   File "main.py", line 372, in classify
-#     classify_frame = input_classify()
-#   File "main.py", line 252, in input_classify
-#     bounds = list(map(int, input(bounds_str).split()))
-# NameError: name 'bounds_str' is not defined
+# TODO Skuteczność detekcji mAP = 0.465 (5/6) (2/2)
 
-# TODO Skuteczność detekcji mAP = 0.0 (0/6) (0/2)
+# TODO max(5, 3+2) = 5
 
-# TODO max(0, 0+0) = 0
+# TODO Poprawki po terminie. (-1)
 
 # file paths
 anno_test_path = "../test/annotations/"
